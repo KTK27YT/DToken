786077e58c888425edc22ee92e5953e0f2a66a11
Kartik <kartiksharmahkg852@gmail.com>
2023-07-09 18:29:02 +0800

    Faucet Functionality add

--- src/DSurv/main.mo
+++ src/DSurv/main.mo
@@ -1,5 +1,6 @@
 import Principal "mo:base/Principal";
 import HashMap "mo:base/HashMap";
+import Debug "mo:base/Debug";
 
 actor Token {
   // Run dfx identity get-principal to get your principal id
@@ -26,4 +27,15 @@ actor Token {
   public query func getSymbol() : async Text {
     return symbol;
   };
+
+  public shared (msg) func payOut() : async Text {
+    // Debug.print(debug_show (msg.caller));
+    if (balances.get(msg.caller) == null) {
+      let amount = 10000;
+      balances.put(msg.caller, amount);
+      return "Success";
+    };
+    return "Already Claimed";
+  }
+
 };
--- src/DSurv_assets/src/components/Faucet.jsx
+++ src/DSurv_assets/src/components/Faucet.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { DSurv } from "../../../declarations/DSurv";
 function Faucet() {
-
+  const [isDisabled, setDisabled] = useState(false);
+  const [buttonText, setText] = useState("Gimme gimme");
   async function handleClick(event) {
-
+    setDisabled(true);
+    const result = await DSurv.payOut();
+    setText(result);
   }
 
   return (
@@ -16,8 +19,9 @@ function Faucet() {
       </h2>
       <label>Get your free DANG tokens here! Claim 10,000 DANG coins to your account.</label>
       <p className="trade-buttons">
-        <button id="btn-payout" onClick={handleClick}>
-          Gimme gimme
+        <button id="btn-payout" onClick={handleClick}
+          disabled={isDisabled}>
+          {buttonText}
         </button>
       </p>
     </div>
